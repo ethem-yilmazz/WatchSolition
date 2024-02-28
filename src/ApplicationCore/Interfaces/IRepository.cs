9d3fffc14f28d1fe038e59ce163f9077cacdd7de
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-02-28 12:32:09 +0300

    Katmanlar

---
+++ src/ApplicationCore/Interfaces/IRepository.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Entities;
+using Ardalis.Specification;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Interfaces
+{
+	public interface IRepository<T> where T : BaseEntity
+	{
+		public Task<T?> GetByIdAsync(int id);
+		public Task<List<T>> GetAllAsync(ISpecification<T> specification);
+		public Task<T> AddAsync(T entity);
+		public Task UpdateAsync(T entity);
+		public Task DeleteAsync(T entity);
+		public Task<int> CountAsync(ISpecification<T> specification);
+		public Task<T> FirstAsync(ISpecification<T> specification);
+		public Task<T?> FirstOrDefaultAsync(ISpecification<T> specification);
+
+	}
+}
---
+++ src/Infrastructure/Data/WatchWorldContext.cs
@@ -0,0 +1,23 @@
+﻿using ApplicationCore.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+
+namespace Infrastructure.Data
+{
+	public class WatchWorldContext : DbContext
+	{
+		public WatchWorldContext(DbContextOptions<WatchWorldContext> options) : base(options)
+		{
+
+		}
+		public DbSet<Brand> Brands { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Product> Products { get; set; }
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+		}
+	}
+}
