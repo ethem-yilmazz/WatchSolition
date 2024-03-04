42f3659d090678d08e6e89e7dc2a34509eb147ac
ethem yilmaz <ethem_yilmaz95@icloud.com>
2024-03-04 12:30:03 +0300

    home view modal

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -3,11 +3,14 @@ using Infrastructure.Data;
 using Infrastructure.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Web.Interfaces;
+using Web.Services;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 
 
 builder.Services.AddDbContext<WatchWorldContext>(options=>
@@ -19,6 +22,7 @@ options.UseNpgsql(builder.Configuration.GetConnectionString("AppIdentityDbContex
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddScoped(typeof(IRepository<>),typeof(EfRepository<>));
+builder.Services.AddScoped<IHomeViewModelService,HomeViewModelService>();
 
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = false)
 	.AddRoles<IdentityRole>()
@@ -41,6 +45,8 @@ else
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRequestLocalization("en-US");
 
 app.UseRouting();
 
